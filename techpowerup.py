d6575d28a30b9f6d24a21d8336454daa715774e2
Yingzhou <zyzstat@foxmail.com>
2021-10-18 15:59:10 +0800

    Updates

--- techpowerup.py
+++ techpowerup.py
@@ -13,28 +13,33 @@ def handle_request(url):
 
 
 def get_text(href):
-    request = handle_request(href)
-    content = urllib.request.urlopen(request).read().decode("utf8")
-    soup = BeautifulSoup(content, "lxml")
-    titlediv = soup.find_all("a", class_="newslink")
-    contdiv = soup.find_all("div", class_="text p")
-    with open("./tpu_txt.txt", "w") as writer:
-        writer.write("------------------TechPowerUp!!!------------------\n\n\n\n\n\n\n\n")
-        for cont, cont_01 in zip(titlediv, contdiv):
-            title = cont.text
-            content = cont_01.text
-            content = content.replace("Read Review", "")
-            content = content.replace("Read full story", "")
-            writer.write("------------------News------------------\n")
-            writer.write(title.strip("\n").strip())
-            # writer.write("\n----------------------------------------\n")
-            writer.write("\n\n\n")
-            # writer.write("-----------------Content----------------\n")
-            writer.write(content.strip("\n").strip())
-            writer.write("\n---------------------------------------\n")
-            writer.write("\n\n\n")
+    try:
+        print("Wait for it")
+        request = handle_request(href)
+        content = urllib.request.urlopen(request).read().decode("utf8")
+        soup = BeautifulSoup(content, "lxml")
+        titlediv = soup.find_all("a", class_="newslink")
+        contdiv = soup.find_all("div", class_="text p")
+        with open("./tpu_txt.txt", "w") as writer:
+            writer.write(
+                "------------------TechPowerUp!!!------------------\n\n\n"
+            )
+            for cont, cont_01 in zip(titlediv, contdiv):
+                title = cont.text
+                content = cont_01.text
+                content = content.replace("Read Review", "")
+                content = content.replace("Read full story", "")
+                writer.write("------------------News------------------\n")
+                writer.write(title.strip("\n").strip())
+                # writer.write("\n----------------------------------------\n")
+                writer.write("\n\n\n")
+                # writer.write("-----------------Content----------------\n")
+                writer.write(content.strip("\n").strip())
+                writer.write("\n---------------------------------------\n")
+                writer.write("\n\n\n")
+        print("\nEnjoy it\n:)")
+    except Exception as e:
+        print("Error:", e)
 
 
 get_text("http://techpowerup.com")
-
-print("\nEnjoy it\n:)")
